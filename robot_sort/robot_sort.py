cb3f248a50b6637cdc01231e7abd76ddd26e89b5
MurphDirt879 <boxert879@hotmail.com>
2020-05-22 12:23:28 -0400

    Reup

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -96,12 +96,12 @@ class SortingRobot:
         """
         Sort the robot's list.
         """
-        #     l = len(arr)
-    # for i in range(l -1 ):
+    #  l = len(arr)
+    #   for i in range(l -1 ):
     #     for j in range(0, l-i-1):
     #         if arr[j] > arr[j+1]:
     #             arr[j], arr[j+1] = arr[j+1], arr[j]
-    # return arr 
+    #   return arr 
         while self.can_move_right():
             self.swap_item()
             self.move_right()
